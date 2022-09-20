c8485090218150b8707c76778119aadc096c8826
Svetlana Tantsura <tantsura_s99@mail.ru>
2022-09-20 19:54:48 +0300

    Add program

--- Program.cs
+++ Program.cs
@@ -1,10 +1,38 @@
-﻿string[] mas = { "Россия", "казань", "три", "3" };
-int number = 0;
+﻿// Способ с использованием классовых методов 
+// string[] mas = { "Россия", "Казань", "три", "3" };
+// string[] result = Array.FindAll(mas, word => word.Length <= 3);
+
+// Ручной способ
+string[] mas = { "Россия", "Казань", "три", "3" };
+int arrayPoints = 0;
+
 
 for (int i = 0; i < mas.Length; i++)
 {
     if (mas[i].Length <= 3)
     {
+        arrayPoints++;
+    };
+}
 
-    }
+string[] result = new string[arrayPoints];
+
+for (int i = 0; i < mas.Length; i++)
+{
+    if (mas[i].Length <= 3)
+    {
+        for (int j = 0; j < result.Length; j++)
+        {
+            if (result[j] == null)
+            {
+                result[j] = mas[i];
+                break;
+            };
+        }
+    };
+}
+
+for (int i = 0; i < result.Length; i++)
+{
+    Console.Write(result[i] + " ");
 }
